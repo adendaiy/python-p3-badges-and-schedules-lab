f99198c185fbf21a1d9494744ba3ab6e2c5e95e3
adendaiy <adenabdib@gmail.com>
2024-08-20 15:15:35 +0100

    this is my forloop lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges =[]
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for i in range(len(names)):
+        room_number = i + 1
+        room_assignments.append(f"Hello, {names[i]}! You'll be assigned to room {room_number}!")
+    return room_assignments
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    room_assignments = assign_rooms(names)
+    for assignment in room_assignments:
+        print(assignment)
